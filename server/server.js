89d006aca9290823917eb80ea22effc9554b9cfe
cerramaximiliano <cerramaximiliano@gmail.com>
2022-01-27 18:36:42 -0300

    v.1.0.0 (create server)

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+require('./config/config');
+const http = require('http');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cron = require('node-cron');
+app.use(cors());
+
+
+mongoose.connect(process.env.URLDB, {useNewUrlParser: true, useUnifiedTopology: true}, (err, res) => {
+    if(err) throw err;
+    console.log('Base de datos ONLINE');
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando el puerto', process.env.PORT);
+
+});
+
+
+
+
+
