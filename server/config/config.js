733a8cf3491d91f06628a77f080cd055a25d6b58
cerramaximiliano <cerramaximiliano@gmail.com>
2022-06-26 13:17:21 -0300

    v1.01

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,4 @@
 process.env.PORT = process.env.PORT || 8080;
-urlDB = 'mongodb+srv://maxcerra:988703ab@cluster0.ueaes.azure.mongodb.net/cafe?retryWrites=true&w=majority';
-process.env.URLDB = urlDB;
+// urlDB = 'mongodb+srv://maxcerra:988703ab@cluster0.ueaes.azure.mongodb.net/cafe?retryWrites=true&w=majority';
+// process.env.URLDB = urlDB;
 process.env.MAIL_MAILGUN_PASS = '07c3fd103285dc49390980456a789cf5-27a562f9-d90ad04c';
