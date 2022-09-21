4df58c74cd4672df929a81de0be88a53c5d79e5f
cerramaximiliano <cerramaximiliano@gmail.com>
2022-09-21 17:59:13 -0300

    v1.03

--- public/home.js
+++ public/home.js
@@ -2,7 +2,7 @@
 window.addEventListener("load", () => {
 
 const modalEmails = new bootstrap.Modal(document.querySelector('#modal-EmailMarketing'));
-
+const forms = document.querySelector('.needs-validation');
 
 
 document.querySelector('#updateData').addEventListener('click', (e) => {
@@ -42,64 +42,57 @@ document.querySelector("#add-Emails").addEventListener('click', (e) => {
     modalEmails.show();
 });
 document.querySelector("#add-EmailsList").addEventListener('click', (e) => {
-    const emails = document.querySelector("#emailsArea").value;
-    const select = document.querySelector("#selectType");
-    const optionSelected = select.options[select.selectedIndex].value;
-    const emailsList = Papa.parse(emails);
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-    var urlencoded = new URLSearchParams();
-    urlencoded.append("email", JSON.stringify(emailsList.data));
-    urlencoded.append("type", optionSelected);
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: urlencoded,
-      redirect: "follow",
-    };
-    fetch('/emailpromotion', requestOptions)
-    .then((res) => res.json())
-    .then((result) => {
-        if(result.err != undefined){
-            if(result.err.code === 11000){
-                toastr.error(`<p>
-                            Items totales: ${result.err.result.insertedIds.length}
-                            </p>
-                            <p>
-                            Items insertados sin error: ${result.err.result.nInserted}
-                            </p>
-                            <p>
-                            Items no insertados: ${result.err.writeErrors.length}
-                            </p>
-                            `
-                )
-
-
+    if(forms.checkValidity() === true){
+        const emails = document.querySelector("#emailsArea").value;
+        const select = document.querySelector("#selectType");
+        const optionSelected = select.options[select.selectedIndex].value;
+        const emailsList = Papa.parse(emails);
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+        var urlencoded = new URLSearchParams();
+        urlencoded.append("email", JSON.stringify(emailsList.data));
+        urlencoded.append("type", optionSelected);
+        var requestOptions = {
+          method: "POST",
+          headers: myHeaders,
+          body: urlencoded,
+          redirect: "follow",
+        };
+        fetch('/emailpromotion', requestOptions)
+        .then((res) => res.json())
+        .then((result) => {
+            console.log(result)
+            if(result.err != undefined){
+                if(result.err.code === 11000){
+                    toastr.error(`<p>
+                                Items totales: ${result.err.result.insertedIds.length}
+                                </p>
+                                <p>
+                                Items insertados sin error: ${result.err.result.nInserted}
+                                </p>
+                                <p>
+                                Items no insertados: ${result.err.writeErrors.length}
+                                </p>
+                                `
+                    )
+    
+    
+                }
+            }else{
+                toastr.success(`Items insertados sin error: ${result.result.length}`)
             }
-        }else{
-            toastr.success(`Items insertados sin error: ${result.result.length}`)
-        }
-        modalEmails.hide();
-    })
-    .catch((err) => {
-        console.log(err)
-    });
+            modalEmails.hide();
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+    }else{
+        document.querySelector('#validateEmail').classList.add('was-validated');
+    }
 });
 
 document.querySelector('#view-Email').addEventListener('click',function(e) {
     window.open('/emailusers', '_self');
-    // var requestOptions = {
-    //   method: "GET",
-    //   redirect: "follow",
-    // };
-    // fetch('/emailusers', requestOptions)
-    // .then((res) => res.json())
-    // .then((results) => {
-    //     console.log(results)
-    // })
-    // .catch((err) => {
-    //     console.log(err)
-    // })
 });
 
 
