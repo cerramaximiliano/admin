fa28fe323fe9bb69df639da380714a07a534bfd4
cerramaximiliano <cerramaximiliano@gmail.com>
2022-09-30 08:19:27 -0300

    v1.05

--- server/config/pino.js
+++ server/config/pino.js
@@ -1,14 +1,19 @@
 const pino = require('pino');
-// const pino = require('pino-pretty');
 const logger = pino({
     transport: {
     targets :[
+        {
+        target: 'pino-pretty',
+        options: {
+        colorize: true,
+        translateTime: 'dd-mm-yyyy, HH:MM:ss',
+        }},
         {
             target: 'pino-pretty',
             options: {
             colorize: false,
             translateTime: 'dd-mm-yyyy, HH:MM:ss',
-            destination: `./logger.log`
+            destination: `./server/logger.log`
             }},
     ]
 },
