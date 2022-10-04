da2887eeff7d86a5dfc5d1e287b990295212d678
cerramaximiliano <cerramaximiliano@gmail.com>
2022-10-04 18:59:55 -0300

    v1.05

--- server/controllers/filesController.js
+++ server/controllers/filesController.js
@@ -1,9 +1,10 @@
 const fileConfig = require('../config/files.js');
 const fs = require('fs');
 const path = require('path');
+const pathFiles = path.join(__dirname, '../');
+const pathServer = pathFiles;
 
 exports.getNames = async (req, res, next) => {
-
     const resultNames = await fileConfig.getNameFiles('./server/files/serverFiles/tasa_pasiva_BNA');
     console.log(resultNames)
     return res.status(200).json({
@@ -13,7 +14,7 @@ exports.getNames = async (req, res, next) => {
 };
 
 exports.getLogger = (req, res, next) => {
-    fs.readFile(path.join('./server/' ,'logger.log'), 'utf8', function(err, data) {
+    fs.readFile(path.join(pathServer, 'logger.log'), 'utf8', function(err, data) {
         if (err){
             return res.status(500).json({
                 ok: false,
@@ -21,7 +22,7 @@ exports.getLogger = (req, res, next) => {
                 status: 500
             })
         }
-        return res.status(202).json({
+        return res.status(200).json({
             ok: false,
             status: 200,
             data: data
