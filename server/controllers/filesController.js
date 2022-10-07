f90dd70da49c2292f42b0fe82d606809849ea889
cerramaximiliano <cerramaximiliano@gmail.com>
2022-10-07 08:44:19 -0300

    v1.05

--- server/controllers/filesController.js
+++ server/controllers/filesController.js
@@ -40,7 +40,7 @@ exports.getLoggerApp = async (req, res, next) => {
         body: JSON.stringify(body),
         headers: {'Content-Type': 'application/json'}
     };
-    const response = await fetch('http:/www.lawanalytics.app/logger-app', options);
+    const response = await fetch('https://www.lawanalytics.app/logger-app', options);
     console.log(response)
     const data = await response.json();
     return  res.status(data.status).json({
