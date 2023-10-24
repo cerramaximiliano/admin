1cb320dfa0814158449c3461e3ce7e4386b94caa
cerramaximiliano <cerramaximiliano@gmail.com>
2023-10-24 09:24:57 -0300

    fix/puppeteer path

--- server/config/puppeteer.js
+++ server/config/puppeteer.js
@@ -5,7 +5,7 @@ const chromeOptions =
     defaultViewport: null,
     args: ['--no-sandbox'],
     ignoreDefaultArgs: ["--disable-extensions"],
-    executablePath: '/admin/node_modules/puppeteer/.local-chromium/linux-982053/chrome-linux/chrome',
+    executablePath: '/usr/bin/google-chrome-stable',
   };
 
 module.exports = chromeOptions;
